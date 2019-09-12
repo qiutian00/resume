c51fc342a9547d45263be15ab9adea5bc21ed8c2
qiutian00 <1476102147@qq.com>
2019-09-12 12:12:44 +0800

    feat: add build tools

---
+++ build.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const marked = require('marked');
+
+const resume = fs.readFileSync(path.join(__dirname, './README.md'), 'utf8');
+const content = marked(resume);
+
+const tmpl = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Harry Yu - Senior Front-End Developer</title>
+    <link rel="stylesheet" href="./styles.css" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-57557739-4"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-57557739-4');
+    </script>
+  </head>
+  <body>
+    ${content}
+  </body>
+</html>
+`;
+
+fs.writeFileSync(path.join(__dirname, './index.html'), tmpl, 'utf8');
